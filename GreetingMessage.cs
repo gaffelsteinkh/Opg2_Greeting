0334f731f6c495ed8e6d2006e076d78dd5eedd69
gaffelsteinkh <GaffelsteinKH@gmail.com>
2026-01-27 14:42:40 +0100

    Added DayOfWeek

--- GreetingMessage.cs
+++ GreetingMessage.cs
@@ -7,25 +7,25 @@ public class TimeCheck
         string? name = Console.ReadLine();
         Console.WriteLine("\n");
 
-        
+        string day = DateTime.Now.DayOfWeek.ToString();
         string time = DateTime.Now.ToString("HH:mm");
-        //Console.WriteLine(time);
+        //Console.WriteLine(day);
         int hour = DateTime.Now.Hour;
         if (hour < 6)
         {
-            Console.WriteLine($"Go to bed! {name}\nCurrent time is: {time}");
+            Console.WriteLine($"Go to bed! {name}\nIt's {day}, {time}");
         }
         else if (hour < 12)
         {
-            Console.WriteLine($"Good morning, {name}\nCurrent time is: {time}");
+            Console.WriteLine($"Good morning, {name}\nIt's {day}, {time}");
         }
         else if (hour < 16)
         {
-            Console.WriteLine($"Good afternoon, {name}\nCurrent time is: {time}");
+            Console.WriteLine($"Good afternoon, {name}\nIt's {day}, {time}");
         }
         else if (hour < 24)
         {
-            Console.WriteLine($"Good evening, {name}\nCurrent time is: {time}");
+            Console.WriteLine($"Good evening, {name}\nIt's {day}, {time}");
         }
 
     }
